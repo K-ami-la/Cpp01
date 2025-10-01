3b10d93aadc0959f2bd5d5aa750e8be9dd846ed9
Kamilla Aizatova <kaizatov@f6r8s15.clusters.42paris.fr>
2025-10-01 22:56:00 +0200

    heeeey

---
+++ ex06/Harl.hpp
@@ -0,0 +1,32 @@
+// Header-protection
+#pragma once
+
+// Includes
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+// classes
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+	// Constructors
+		Harl();
+
+	// Deconstructors
+		~Harl();
+
+	// Public Methods
+		void	complain(std::string level);
+	// Getter
+
+	// Setter
+
+};
